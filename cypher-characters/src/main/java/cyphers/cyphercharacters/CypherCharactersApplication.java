7f6a808118ff692c26c9ca72d1e565998224a38e
cellenar26 <cellenar26@gmail.com>
2020-10-07 16:15:28 +0900

    Initial commit

---
+++ cypher-characters/src/main/java/cyphers/cyphercharacters/CypherCharactersApplication.java
@@ -0,0 +1,13 @@
+package cyphers.cyphercharacters;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CypherCharactersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CypherCharactersApplication.class, args);
+	}
+
+}
---
+++ cypher-characters/src/test/java/cyphers/cyphercharacters/CypherCharactersApplicationTests.java
@@ -0,0 +1,13 @@
+package cyphers.cyphercharacters;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CypherCharactersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

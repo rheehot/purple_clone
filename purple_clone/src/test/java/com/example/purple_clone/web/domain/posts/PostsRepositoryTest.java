0bc83999a63fb51380a2161160d5afe1c1404c47
cellenar26 <cellenar26@gmail.com>
2020-10-21 21:22:09 +0900

    done mustache test

--- purple_clone/src/test/java/com/example/purple_clone/web/domain/posts/PostsRepositoryTest.java
+++ purple_clone/src/test/java/com/example/purple_clone/web/domain/posts/PostsRepositoryTest.java
@@ -2,6 +2,7 @@ package com.example.purple_clone.web.domain.posts;
 
 import com.example.purple_clone.domain.posts.Posts;
 import com.example.purple_clone.domain.posts.PostsRepository;
+import org.assertj.core.api.Assertions;
 import org.junit.After;
 import org.junit.Assert;
 import org.junit.Test;
@@ -10,6 +11,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 @RunWith(SpringRunner.class)
@@ -46,5 +48,27 @@ public class PostsRepositoryTest {
         Assert.assertEquals(posts.getTitle(), title);
         Assert.assertEquals(posts.getContent(), content);
 
+    }
+
+    @Test
+    public void BaseTimeEntity_등록() {
+        // given
+        LocalDateTime now = LocalDateTime.of(2019,6,4,0,0,0);
+        postsRepository.save(Posts.builder()
+                .title("title")
+                .content("content")
+                .author("author")
+                .build());
+        // when
+        List<Posts> postsList = postsRepository.findAll();
+
+        // then
+        Posts posts = postsList.get(0);
+
+        System.out.println(">>>>>>>>>> createDate = "+posts.getCreatedTime()+", modifiedDate="+posts.getModifiedDate());
+
+        Assertions.assertThat(posts.getCreatedTime()).isAfter(now);
+        Assertions.assertThat(posts.getModifiedDate()).isAfter(now);
+//        Assertions.assertThat(posts.getModifiedDate()).isBefore(now);
     }
 }
